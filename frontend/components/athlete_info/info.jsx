de44ead14f4bc68073d35917bb8d89e168379d7e
cmmeur01 <chrismeurer@gmail.com>
2019-04-26 00:50:42 -0400

    all info displaying on athlete info container

--- frontend/components/athlete_info/info.jsx
+++ frontend/components/athlete_info/info.jsx
@@ -6,16 +6,62 @@ class AthleteInfo extends React.Component {
   }
 
   render() {
+    const round = (value, decimals) => (Number(Math.round(value + 'e' + decimals) + 'e-' + decimals));
+    const dateCompare = (a, b) => {
+      if (a.date > b.date) {
+        return 1;
+      } else if (a.date < b.date) {
+        return -1;
+      } else {
+        return 0;
+      }
+    };
     let username = this.props.athlete[parseInt(this.props.currentUserId)].username;
+    let workouts = Object.values(this.props.workouts);
+    let runs = [];
+    let bikes = [];
+    for(let i = 0; i < workouts.length; i++) {
+      if (workouts[i].sport === "Cycling") {
+        bikes.push(workouts[i]);
+      } else {
+        runs.push(workouts[i]);
+      }
+    }
+    let bikeDist = 0;
+    for(let i = 0; i < bikes.length; i++) {
+      if (this.props.routes[bikes[i].route_id] !== undefined) {
+        bikeDist = bikeDist + this.props.routes[bikes[i].route_id].distance;
+      }
+    }
+    let runDist = 0;
+    for (let i = 0; i < runs.length; i++) {
+      if (this.props.routes[runs[i].route_id] !== undefined) {
+        runDist = runDist + this.props.routes[runs[i].route_id].distance;
+      }
+    }
+    //oldest -> newest
+    bikes.sort(dateCompare);
+    runs.sort(dateCompare);
+    let runDate;
+    if (runs.length !== 0) {
+      runDate = runs[runs.length - 1].date;
+    }
+    let bikeDate;
+    if (bikes.length !== 0) {
+      bikeDate = bikes[bikes.length - 1].date;
+    }
     return (
       <div className="athlete-info-container">
         <h1>{username}</h1>
-        <div className="work-route-disp">
-        <div>Workouts</div>
-        <div>Routes</div>
+        <div className="work-route-disp">Total Workouts
+          <div className="wo-inner-disp"><div>Running: {runs.length}</div><div>Cycling: {bikes.length}</div></div>
         </div>
-        <div className="last-work">Last Workout</div>
-        <div className="work-link">Your Workouts</div>
+        <div className="work-route-disp">Total Distance
+          <div className="wo-inner-disp"><div>Running: {round(runDist/1000,2)}km</div><div>Cycling: {round(bikeDist/1000,2)}km</div></div>
+        </div>
+        <div className="work-route-disp">Last Workout
+          <div className="wo-inner-disp"><div>Running: {runDate}</div><div>Cycling: {bikeDate}</div></div>
+        </div>
         </div>
     );
   }
--- frontend/components/athlete_info/info_container.js
+++ frontend/components/athlete_info/info_container.js
@@ -3,7 +3,9 @@ import { connect } from 'react-redux';
 
 const msp = (state, ownProps) => ({
   athlete: state.entities.users,
-  currentUserId: state.session.currentUserId
+  currentUserId: state.session.currentUserId,
+  routes: state.entities.routes, 
+  workouts: state.entities.workouts
 });
 
 const mdp = dispatch => ({
