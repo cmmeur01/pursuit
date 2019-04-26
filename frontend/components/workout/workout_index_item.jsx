8176b45e5734b68b68eef47ed23f7503f3570fa0
cmmeur01 <chrismeurer@gmail.com>
2019-04-26 13:18:58 -0400

    workout index item styling

--- frontend/components/workout/workout_index_item.jsx
+++ frontend/components/workout/workout_index_item.jsx
@@ -28,18 +28,23 @@ class WorkoutIndexItem extends React.Component {
     const round = (value, decimals) => (Number(Math.round(value + 'e' + decimals) + 'e-' + decimals));
     let hours = Math.floor(this.props.workout.duration/60);
     let minutes = this.props.workout.duration % 60;
+    let type;
+    if (this.props.workout.sport === "Cycling") {
+      type = "rode";
+    } else {
+      type = "ran";
+    }
     return (<div className="wo-idx-item" id="wo-idx-item">
       <li>
         <div className="top-wo-items">
-          <h5>{this.props.users[this.props.workout.user_id].username}</h5>
-          <h6>On: {this.props.workout.date}</h6>
+          <h6>{this.props.users[this.props.workout.user_id].username} {type} on: {this.props.workout.date}</h6>
           <h2>{this.props.workout.title}</h2>
           <h5>{this.props.workout.description}</h5>
         </div>
         <div className="bottom-wo-items">
-          <div className="wo-show-item"><h6>Distance</h6><div className="wo-inner"><h4>{round(this.props.route.distance / 1000, 2)}</h4><h6>km</h6></div></div>
-          <div className="wo-show-item"><h6>Elevation</h6><div className="wo-inner"><h4>{round(this.props.route.elevation, 1)}</h4><h6>m</h6></div></div>
-          <div className="wo-show-item"><h6>Duration</h6><div className="wo-inner"><h4>{hours}</h4><h6>h</h6> <h4>{minutes}</h4><h6>m</h6></div></div>
+          <div className="wo-show-item"><h4>Distance:{round(this.props.route.distance / 1000, 2)}km</h4></div>
+          <div className="wo-show-item"><h4>Elevation:{round(this.props.route.elevation, 1)}m</h4></div>
+          <div className="wo-show-item"><h4>Duration:{hours}h{minutes}m</h4></div>
         </div>
         <div className='wo-idx-item-map' id='workout-idx-item-map' ref={map => this.mapNode = map}>
         </div>
